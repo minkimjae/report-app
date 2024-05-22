3202c86a0bb14d9d225523d6a9c7454c6e836dd2
kmjhome <trg710@gmai.com>
2024-05-23 01:39:50 +0900

    chore : 데이터베이스 추가를 위한 room 의존성 추가

--- ReportApp/app/build.gradle.kts
+++ ReportApp/app/build.gradle.kts
@@ -29,14 +29,22 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+   viewBinding {
+       enable = true
+   }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // room db
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
