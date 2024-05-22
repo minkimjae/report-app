89c6dd1989eb2b8c321a52bc6568a251491f3b85
kmjhome <trg710@gmai.com>
2024-05-23 01:55:44 +0900

    fix : entity 컬럼들 public으로 접근제어 변경
    private로 선언시 Room에서 해당 변수에 접근이 불가 하여 오류가 남

--- ReportApp/app/src/main/java/com/software/reportapp/db/entity/Contact.java
+++ ReportApp/app/src/main/java/com/software/reportapp/db/entity/Contact.java
@@ -6,10 +6,10 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Contact {
     @PrimaryKey(autoGenerate = true) // DB 값 추가 시 자동으로 +1 증가된 값 삽입
-    private int id; // PK
-    private String name; // 이름
-    private String phoneNumber; // 연락처
-    private String email; // 이메일
-    private String workPlace; // 직장명
-    private String title; // 직합
+    public int id; // PK
+    public String name; // 이름
+    public String phoneNumber; // 연락처
+    public String email; // 이메일
+    public String workPlace; // 직장명
+    public String title; // 직합
 }
