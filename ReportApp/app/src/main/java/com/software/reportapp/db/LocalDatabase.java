f82e10a08c3d0412c7528247d1b868c7eb282a1d
kmjhome <trg710@gmai.com>
2024-05-25 08:39:12 +0900

    feat : Database 싱글톤으로 가져오기 위한 소스코드 추가
    데이터베이스의 인스턴스가 한번만 생성 되는 것을 보장하는 코드

--- ReportApp/app/src/main/java/com/software/reportapp/db/LocalDatabase.java
+++ ReportApp/app/src/main/java/com/software/reportapp/db/LocalDatabase.java
@@ -1,6 +1,9 @@
 package com.software.reportapp.db;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.software.reportapp.db.dao.ContactDao;
@@ -8,5 +11,22 @@ import com.software.reportapp.db.entity.Contact;
 
 @Database(entities = {Contact.class}, version = 1)
 public abstract class LocalDatabase extends RoomDatabase {
+    private static volatile LocalDatabase INSTANCE;
     public abstract ContactDao dao();
+
+    public static LocalDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (LocalDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    LocalDatabase.class, "coin_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
 }
