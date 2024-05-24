12781baee91c937b00a6804b30a7189ba52c5a54
kmj9563 <kmj9563@gmail.com>
2024-05-24 18:21:28 +0900

    feat : Room DAO 구현

--- ReportApp/app/src/main/java/com/software/reportapp/db/dao/ContactDao.java
+++ ReportApp/app/src/main/java/com/software/reportapp/db/dao/ContactDao.java
@@ -1,7 +1,26 @@
 package com.software.reportapp.db.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.software.reportapp.db.entity.Contact;
+
+import java.util.List;
 
 @Dao
 public interface ContactDao {
+    @Insert
+    void insert(Contact contact);
+
+    @Query("SELECT id, name FROM Contact")
+    LiveData<List<Contact>> getContactList();
+
+    @Query("SELECT * FROM Contact WHERE id = :id")
+    LiveData<Contact> getContactById(int id);
+
+    @Delete
+    int delete(Contact contact);
 }
