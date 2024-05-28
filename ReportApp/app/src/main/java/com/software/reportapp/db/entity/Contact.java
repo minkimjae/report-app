8178928a24e2f1d47a94ff84fe5f1a8ec7cc2ec8
kmj9563 <kmj9563@gmail.com>
2024-05-28 17:00:22 +0900

    feat : 값 추가를 위해 getter, setter 추가

--- ReportApp/app/src/main/java/com/software/reportapp/db/entity/Contact.java
+++ ReportApp/app/src/main/java/com/software/reportapp/db/entity/Contact.java
@@ -12,4 +12,53 @@ public class Contact {
     public String email; // 이메일
     public String workPlace; // 직장명
     public String title; // 직합
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
